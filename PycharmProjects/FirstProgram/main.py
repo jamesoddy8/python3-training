73c85754e6e2ea4360003ba4432443d7db3437aa
James Oddy <jamesoddy8@gmail.com>
2021-01-26 13:09:59 +0000

    adding other files to repo

---
+++ PycharmProjects/FirstProgram/main.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+print(int("5") + int("7"))
+
+# how to convert string to integer - wrap it in int(" ")
+
+# convert text to integer doesn't work - int("hello") will throw an error
+
+# for strings where you need to include "", wrap in single ' quotes - 'She said "I want this"'
+# 'She said "Don't put that there"' will throw an error
+
+# 'She said "Don\'t Do That"'
+# What "\" does is tells the interpreter to read the following character as a string - so "don\'t" would return "don't"
+
+
+
